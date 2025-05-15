db252b9a77c6d8dcbfff62407eb7307f63d5bf4d
Hugo Lopes <hugofslopes@live.com.pt>
2025-05-15 20:03:13 +0100

    First commmit

---
+++ src /Fixed.cpp
@@ -0,0 +1,17 @@
+#include "../includes/Fixed.hpp"
+
+Fixed::Fixed(){
+
+}
+
+Fixed::~Fixed(){
+    
+}
+
+int Fixed::getRawBits( void ) const{
+
+}
+
+void Fixed::setRawBits( int const raw ){
+    
+}    
